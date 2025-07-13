c544f07fbd6edd3b72e0f411d00f1af588d6bc28
Aymen-Ben-Rached <aymen.benrached@esprit.tn>
2025-07-13 23:41:44 +0100

    initial commit

---
+++ src/main/java/com/esprit/articlestock/ArticleStockApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.articlestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esprit/articlestock/ArticleStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.articlestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
